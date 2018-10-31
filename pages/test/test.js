1822d34ea4381d4eeba66c1c267f97a75f9649fb
ssd <1170909625@qq.con>
2018-10-31 11:31:00 +0800

    小程序的第一次提交

---
+++ pages/test/test.js
@@ -0,0 +1,18 @@
+var app = getApp()
+Page({
+  data: {
+    imgUrls: [
+      '/images/1.jpg',
+      '/images/2.jpg',
+      '/images/3.jpg',
+      '/images/4.jpg',
+      '/images/5.jpg'
+    ],
+    
+    indicatorDots: true,
+    autoplay: true,
+    interval: 3000,
+    duration: 200,
+ 
+  },
+})
